982b93fe38510526beb1912b962bcf0c5568aee3
Osama Oransa <osa_ora@yahoo.com>
2023-05-21 12:03:44 +0400

    Create db-scripts.sql

---
+++ mysql-example/db-scripts.sql
@@ -0,0 +1,3 @@
+create table accountdb.account ( id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, firstname VARCHAR( 255 ) NOT NULL, lastname VARCHAR( 255 ) NOT NULL,status INT NOT NULL);
+insert into accountdb.account (id, firstname, lastname, status) values (1,'Osama','Oransa',1);
+insert into accountdb.account (id, firstname, lastname, status) values (2,'Osa','Ora',1);
