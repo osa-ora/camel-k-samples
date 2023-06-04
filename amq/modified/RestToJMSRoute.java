33362a04a6cb837730d0401f0cc25da92d64c9b0
Osama Oransa <osa_ora@yahoo.com>
2023-06-04 15:45:02 +0300

    Create RestToJMSRoute.java

---
+++ amq/modified/RestToJMSRoute.java
@@ -0,0 +1,24 @@
+// camel-k: language=java
+// camel-k: dependency=mvn:org.amqphub.quarkus:quarkus-qpid-jms
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+
+public class RestToJMSRoute extends RouteBuilder {
+
+   @Override
+   public void configure() throws Exception {
+   
+        // Define REST API endpoint
+        rest("/send")
+            .get("/{message}")
+            .to("direct:sendMessage");
+
+        // Define the route to retrieve user account details from the database
+        from("direct:sendMessage")
+            .setHeader("Content-Type", constant("application/json"))
+            .setBody(simple("{'message':'${header.message}'}"))
+            .to("jms:{{jms.destinationType}}:{{jms.destinationName}}?exchangePattern=InOnly")
+            .log("Message details: ${body}");
+   }
+}
