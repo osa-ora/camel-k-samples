353a1de81c8e86f8976eafd094e91fc36378ee4e
Osama Oransa <osa_ora@yahoo.com>
2023-06-10 13:49:42 +0300

    Create KafkaProducer.java

---
+++ kafka-sample/KafkaProducer.java
@@ -0,0 +1,23 @@
+// camel-k: language=java dependency=mvn:org.apache.camel.quarkus:camel-quarkus-kafka dependency=mvn:io.strimzi:kafka-oauth-client:0.7.1.redhat-00003
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+
+public class KafkaProducer extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        log.info("Kafak Producer is ready!");
+        // Define REST API endpoint
+        rest("/send")
+            .get("/{message}")
+            .to("direct:sendToKafka");
+
+        // Define the route to retrieve user account details from the database
+        from("direct:sendToKafka")
+            .setHeader("Content-Type", constant("application/json"))
+            .setBody(simple("{'message':'${header.message}'}"))
+            .to("kafka:{{producer.topic}}")
+            .log("Message correctly sent to the topic: ${body}");
+    }
+}
